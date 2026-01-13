6a1846de5c53f62c091206576ff4045c347bb787
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2026-01-12 23:41:16 -0500

    (wip) add unrelenter

---
+++ components/nvidia.nix
@@ -0,0 +1,37 @@
+# nvidia config for desktop computers
+{ config, lib, pkgs, ... }:
+{
+	hardware.nvidia = {
+		# nvidia seems to reccommend prod or beta for 20-series and 40-series.
+		package = config.boot.kernelPackages.nvidiaPackages.production;
+		# package = config.boot.kernelPackages.nvidiaPackages.beta;
+
+		# Modesetting is required.
+		modesetting.enable = true;
+
+		# Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+		# Enable this if you have graphical corruption issues or application crashes after waking
+		# up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+		# of just the bare essentials.
+		powerManagement.enable = false;
+
+		# Fine-grained power management. Turns off GPU when not in use.
+		# Experimental and only works on modern Nvidia GPUs (Turing or newer).
+		powerManagement.finegrained = false;
+
+		# https://unix.stackexchange.com/questions/799102/nvidia-proprietary-vs-open-drivers-differences
+		# According to the above, there shouldn't be any functional difference between nvidia's open drivers and the
+		# closed one. I'll have to experiment what works best.
+		open = false;
+
+		# Enable the Nvidia settings menu,
+		# accessible via `nvidia-settings`.
+		nvidiaSettings = true;
+	};
+	# Enable OpenGL
+	hardware.graphics = {
+		enable = true;
+	};
+	# Load nvidia driver for Xorg and Wayland
+	services.xserver.videoDrivers = ["nvidia"];
+}
---
+++ machines/unrelenter.nix
@@ -0,0 +1,32 @@
+# Unrelenter, est. 2023.
+# - R9 7950X3D
+# - 64 GiB of RAM
+{ config, pkgs, ... }: {
+	imports = [
+		./_common.nix
+		../components/boot/secure.nix
+		../components/kernel/latest.nix
+		../components/avahi.nix
+		../components/desktop_devtools.nix
+		../components/desktop.nix
+		../components/devtools.nix
+		../components/nvidia.nix
+		../components/spotify.nix
+		../components/steam.nix
+		../components/hardware_workarounds/keychron.nix
+		../components/hardware_workarounds/qmk.nix
+	];
+	networking.hostName = "unrelenter";
+	users.users.aritz = {
+		isNormalUser = true;
+		description = "Aritz Beobide-Cardinal";
+		extraGroups = [ "networkmanager" "wheel" "dialout" ];
+		packages = with pkgs; [];
+	};
+	boot.kernelParams = [ 
+		# The Intel I225 2.5G LAN on my Asus ROG STRIX X670E-E likes to kill itself after a couple hours without
+		# these params. While that was with Debian 13, I doubt things have changed much since then.
+		"pcie_port_pm=off"
+		"pcie_aspm.policy=performance"
+	];
+}
