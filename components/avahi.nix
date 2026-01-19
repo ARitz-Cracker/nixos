76ba8572119a62941809aeeff430f470641fdb53
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2026-01-19 09:39:00 -0500

    allow for machines running avahi to be discoverable

--- components/avahi.nix
+++ components/avahi.nix
@@ -4,5 +4,12 @@
 		nssmdns4 = true;
 		nssmdns6 = true;
 		openFirewall = true;
+		publish = {
+			enable = true;
+			addresses = true;
+			domain = true;
+			userServices = true;
+			workstation = true;
+		};
 	};
 }
